78611b63373cb694e2f11a3f7ae496ff86b5f0b1
himeno61 <binkiewicz.luke@gmail.com>
2023-06-08 14:38:54 +0200

    Add simple mock test for transaction service

--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TransactionsService } from './transactions.service';
+import { Transaction } from '../typeorm';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('TransactionsService', () => {
   let service: TransactionsService;
+  let repository: Repository<Transaction>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TransactionsService],
+      providers: [
+        TransactionsService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<TransactionsService>(TransactionsService);
+    repository = module.get(getRepositoryToken(Transaction));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be defined', async () => {
+    const now = new Date();
+    const transaction: Transaction = {
+      id: 'mockedId',
+      to: 'mockedTo',
+      from: 'mockedFrom',
+      value: 'mockedValue',
+      createdAt: now,
+      timestamp: now,
+    };
+
+    const findOneSpy = jest
+      .spyOn(repository, 'find')
+      .mockResolvedValue(Array.of(transaction));
+    const transactions = await service.findRecent();
+    expect(transactions).toContain(transaction);
   });
 });
